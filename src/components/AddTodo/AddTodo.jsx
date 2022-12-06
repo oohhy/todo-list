7f8324945089d0a8458335ed7eaaafd4fde3c2dd
hyeyunOh <Ohhyoon0707@gmail.com>
2022-12-06 15:14:13 +0900

    💄 AddTodo 구현

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,7 @@
-// 사용자에게 입력을 받아서 그 입력이 올바른지 아닌지 검사하는 로직
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import styles from "./AddTodo.module.css";
+// 사용자에게 입력을 받아서 그 입력이 올바른지 아닌지 검사하는 로직
 export default function AddTodo({ onAdd }) {
   const [text, setText] = useState("");
   // 이벤트 대상인 text를 input의 value로 지정
@@ -12,19 +14,20 @@ export default function AddTodo({ onAdd }) {
     }
     //submit->onAdd=> handleAdd({ id: "고유한 값", text, status: "active" })실행
     //handleAdd는 prop으로 받은 콜백함수
-    onAdd({ id: "고유한 값", text, status: "active" });
+    onAdd({ id: uuidv4(), text, status: "active" });
     //추가 한 뒤 input창 초기화
     setText("");
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <input
+        className={styles.input}
         type="text"
         placeholder="Add Todo"
         value={text}
         onChange={handleChange}
       />
-      <button>Add</button>
+      <button className={styles.button}>Add</button>
     </form>
   );
 }
